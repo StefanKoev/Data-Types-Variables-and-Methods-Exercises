b71f1f6c93c4c21565aa8a70447082dda6b62963
Stefan Koev <koev.stefan@gmail.com>
2017-11-12 01:17:38 +0200

    Create 05. Wormtest

---
+++ 05.Wormtest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthInMeters = double.Parse(Console.ReadLine());
+            double widthInCentemeters = double.Parse(Console.ReadLine());
+
+            double lenghtInCentimeters = lengthInMeters * 100;
+
+            if (lenghtInCentimeters % widthInCentemeters == 0 || widthInCentemeters == 0)
+            {
+                double product = lenghtInCentimeters * widthInCentemeters;
+                Console.WriteLine($"{product:f2}");
+            }
+            else
+            {
+                double percentage = (lenghtInCentimeters / widthInCentemeters) * 100;
+                Console.WriteLine($"{percentage:f2}%");
+            }
+            
+        }
+    }
+}
