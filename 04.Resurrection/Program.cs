f3db3594aa4856f00d67d0117953754da2972c81
Stefan Koev <koev.stefan@gmail.com>
2017-11-12 00:35:02 +0200

    Create 04. Resurrection

---
+++ 04.Resurrection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Resurrection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+           
+            {
+                long totalLength = long.Parse(Console.ReadLine());
+                decimal totalWidth = decimal.Parse(Console.ReadLine());
+                long wingLength = long.Parse(Console.ReadLine());
+
+                decimal totalYears = (totalLength * totalLength) * (totalWidth + (2 * wingLength));
+                Console.WriteLine(totalYears);
+            }
+            
+        }
+    }
+}
