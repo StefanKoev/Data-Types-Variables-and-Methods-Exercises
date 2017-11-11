78b5a4e87c6dcdf26e8de64b301830d1b516f6b2
Stefan Koev <koev.stefan@gmail.com>
2017-11-11 23:39:07 +0200

    Create 03. Poke Mon

---
+++ 03.Poke-Mon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int halfN = n / 2;
+            int count = 0;
+            while (n >= m)
+            {
+                n -= m;
+
+                if (n == halfN)
+                {
+                    if (y > 0)
+                    {
+                        n /= y;
+                    }
+
+                }
+
+                count++;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(count);
+        }
+    }
+}
