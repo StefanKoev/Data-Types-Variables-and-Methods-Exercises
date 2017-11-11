9724c029ea8a285ad7b1ca70ff8a5ffd45d11e52
Stefan Koev <koev.stefan@gmail.com>
2017-11-11 22:17:33 +0200

    Create 01. Anonymous Downsite

---
+++ 01.Anonymous-Downsite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _01.Anonymous_Dounsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWebsites = int.Parse(Console.ReadLine());
+            int securityKey = int.Parse(Console.ReadLine());
+
+            decimal totalLoss = 0.0m;
+            for (int i = 0; i < numberOfWebsites; i++)
+            {
+                string[] websites = Console.ReadLine().Split();
+
+                string siteName = websites[0];
+                long siteVisit = long.Parse(websites[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(websites[2]);
+
+                decimal siteLoss = siteVisit * siteCommercialPricePerVisit;
+
+                totalLoss += siteLoss;
+
+                Console.WriteLine(siteName);
+            }
+            Console.WriteLine($"Total Loss: {totalLoss:f20}");
+            Console.WriteLine($"Security Token: {BigInteger.Pow(securityKey, numberOfWebsites)}");
+        }
+    }
+}
